d8e5c8da98af9a010c0ab634b08bb74dbccba1b3
Francesco Cassese <francescocassese9@gmail.com>
2026-09-04 16:55:57 +0200

    style: restyle welcome page with bootstrap grid and centered logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,16 @@
-@extends('layouts.app')
+@extends('layouts.guest')
 
 @section('content')
-    <div class="logo logo-xxl">
-        <x-logo />
-    </div>
-    <div class="welcome-subtitle">
-        <p>Una guida curata alle attività commerciali indipendenti e alle botteghe artigiane del territorio.</p>
-    </div>
-    <div class="welcome-actions d-flex">
-        <a class="btn-base btn-login" href="{{ route('login') }}">{{ __('Accedi') }}</a>
-        <a class="btn-base btn-register" href="{{ route('register') }}">{{ __('Registrati') }}</a>
+    <div class="col-md-6 px-4 text-center">
+        <div class="logo logo-xxl">
+            <x-logo />
+        </div>
+        <div class="welcome-subtitle">
+            <p class="fs-5">Una guida curata alle attività commerciali indipendenti e alle botteghe artigiane del territorio.</p>
+        </div>
+        <div class="welcome-actions d-flex justify-content-center">
+            <a class="btn-base btn-login" href="{{ route('login') }}">{{ __('Accedi') }}</a>
+            <a class="btn-base btn-register" href="{{ route('register') }}">{{ __('Registrati') }}</a>
+        </div>
     </div>
 @endsection
