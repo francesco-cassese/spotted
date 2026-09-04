b3ce8c44b321e7ad33489fdc110ff3f2d7b246c6
Francesco Cassese <francescocassese9@gmail.com>
2026-09-04 18:03:23 +0200

    feat: add navbar links to categories and distinctive traits

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -29,6 +29,12 @@
                             @endif
                         @endif
                     @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('categories.index') }}">{{ __('Categorie') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('distinctive-traits.index') }}">{{ __('Tratti distintivi') }}</a>
+                        </li>
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
